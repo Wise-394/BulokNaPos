9ee36854043828dfee85dd2189642b7430e44891
Wise-394 <114738222+Wise-394@users.noreply.github.com>
2024-08-21 18:49:47 +0800

    omsim

--- BulokNaPos/Form1.Designer.vb
+++ BulokNaPos/Form1.Designer.vb
@@ -22,10 +22,74 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        lbUname = New Label()
+        lbPword = New Label()
+        tbUname = New TextBox()
+        tbPass = New TextBox()
+        btnLogin = New Button()
+        SuspendLayout()
+        ' 
+        ' lbUname
+        ' 
+        lbUname.AutoSize = True
+        lbUname.Location = New Point(243, 109)
+        lbUname.Name = "lbUname"
+        lbUname.Size = New Size(60, 15)
+        lbUname.TabIndex = 0
+        lbUname.Text = "Username"
+        ' 
+        ' lbPword
+        ' 
+        lbPword.AutoSize = True
+        lbPword.Location = New Point(243, 139)
+        lbPword.Name = "lbPword"
+        lbPword.Size = New Size(57, 15)
+        lbPword.TabIndex = 1
+        lbPword.Text = "Password"
+        ' 
+        ' tbUname
+        ' 
+        tbUname.Location = New Point(309, 106)
+        tbUname.Name = "tbUname"
+        tbUname.Size = New Size(100, 23)
+        tbUname.TabIndex = 2
+        ' 
+        ' tbPass
+        ' 
+        tbPass.Location = New Point(309, 136)
+        tbPass.Name = "tbPass"
+        tbPass.Size = New Size(100, 23)
+        tbPass.TabIndex = 3
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(320, 165)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(75, 23)
+        btnLogin.TabIndex = 4
+        btnLogin.Text = "Login"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnLogin)
+        Controls.Add(tbPass)
+        Controls.Add(tbUname)
+        Controls.Add(lbPword)
+        Controls.Add(lbUname)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents lbUname As Label
+    Friend WithEvents lbPword As Label
+    Friend WithEvents tbUname As TextBox
+    Friend WithEvents tbPass As TextBox
+    Friend WithEvents btnLogin As Button
 
 End Class
--- BulokNaPos/Form1.vb
+++ BulokNaPos/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lbUname.Click
 
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lbPword.Click
+
+    End Sub
 End Class
